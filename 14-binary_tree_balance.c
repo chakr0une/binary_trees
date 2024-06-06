85ff5cb5242b4db4209ebf4983de05a947475a02
Soufian <soufianbenchakroune7@gmail.com>
2024-06-06 12:16:34 -0700

    fix file 0 and 14

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -7,17 +7,23 @@
  */
 size_t binary_tree_height(const binary_tree_t *tree)
 {
-	if (tree)
+	size_t l = 0;
+	size_t r = 0;
+
+	if (tree == NULL)
 	{
-		size_t  left = 0, right = 0;
-
-		left = tree->left ? 1 + binary_tree_height(tree->left) : 1;
-		right = tree->right ? 1 + binary_tree_height(tree->right) : 1;
-		return ((left > right) ? left : right);
+		return (0);
 	}
-	return (0);
+	else
+	{
+		if (tree)
+		{
+			l = tree->left ? 1 + binary_tree_height_b(tree->left) : 1;
+			r = tree->right ? 1 + binary_tree_height_b(tree->right) : 1;
+		}
+		return ((l > r) ? l : r);
+	}
 }
-
 /**
  * binary_tree_balance - Measures balance factor of a binary tree
  * @tree: is a pointer to the root node of the tree to measure the balance factor
@@ -25,8 +31,13 @@ size_t binary_tree_height(const binary_tree_t *tree)
  */
 int binary_tree_balance(const binary_tree_t *tree)
 {
-	if (tree)
-		return (binary_tree_height(tree->left) - binary_tree_height(tree->right));
+	int right = 0, left = 0, total = 0;
 
-	return (0);
+	if (tree)
+	{
+		left = ((int)binary_tree_height_b(tree->left));
+		right = ((int)binary_tree_height_b(tree->right));
+		total = left - right;
+	}
+	return (total);
 }
